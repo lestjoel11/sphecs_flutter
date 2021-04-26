79e2d95d75a9f05f57cc23bf7ca109f52b76ca97
Lester Joel <lestjoel@gmail.com>
2021-04-27 03:03:06 +0400

    segment options into several arrays

--- lib/services/smartphone_data.dart
+++ lib/services/smartphone_data.dart
@@ -1,6 +1,3 @@
-//TODO:READ DATA FROM CSV
-//TODO: PARAMETER FOR BRAND NAME
-
 import 'dart:collection';
 
 import 'package:csv/csv.dart';
@@ -10,17 +7,17 @@ import 'package:flutter/services.dart';
 class SmartphoneData {
   static List<List<dynamic>> data = [];
   List<String> headers = [];
+  var dataLoad = false;
 
   loadAsset() async {
-    final myData = await rootBundle.loadString("smartphone_data/Apple.csv");
+    final myData = await rootBundle.loadString("smartphone_data/main.csv");
     List<List<dynamic>> table = CsvToListConverter().convert(myData);
     data = table;
     //get headers from csv
     for (var i = 0; i < data[0].length; i++) {
       headers.add(data[0][i]);
     }
-    print(data.length);
-    print("Data Load");
+    dataLoad = true;
   }
 
   //TODO: MAKE IF STATEMENTS FOR EACH FUNCTION CHECKING LENGTH IF APP CRASHES
@@ -41,10 +38,8 @@ class SmartphoneData {
         }
       }
     }
-    print('located');
     final sortedScreenType = new SplayTreeSet.from(screenType);
     screenType.clear();
-    print(sortedScreenType);
     return sortedScreenType.toList();
   }
 }
